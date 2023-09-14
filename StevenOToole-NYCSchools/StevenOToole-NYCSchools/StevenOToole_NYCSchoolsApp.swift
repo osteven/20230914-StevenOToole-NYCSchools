4b6d45574dce856ca870232da89969ba98e5db74
osteven <emadan@mac.com>
2023-09-14 12:55:29 -0700

    Restructure

--- StevenOToole-NYCSchools/StevenOToole-NYCSchools/StevenOToole_NYCSchoolsApp.swift
+++ StevenOToole-NYCSchools/StevenOToole-NYCSchools/StevenOToole_NYCSchoolsApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct StevenOToole_NYCSchoolsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
--- StevenOToole-NYCSchools/StevenOToole-NYCSchools/UI/HomeView.swift
+++ StevenOToole-NYCSchools/StevenOToole-NYCSchools/UI/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  StevenOToole-NYCSchools
 //
 //  Created by Steven O'Toole on 9/14/23.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -21,6 +21,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        HomeView()
     }
 }
