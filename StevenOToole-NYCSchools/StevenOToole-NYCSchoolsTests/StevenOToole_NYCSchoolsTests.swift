96b91740124871d717b8d8b6975c0bf1c4122f76
osteven <emadan@mac.com>
2023-09-14 13:54:59 -0700

    Begin HighSchool

--- StevenOToole-NYCSchools/StevenOToole-NYCSchoolsTests/StevenOToole_NYCSchoolsTests.swift
+++ StevenOToole-NYCSchools/StevenOToole-NYCSchoolsTests/StevenOToole_NYCSchoolsTests.swift
@@ -9,6 +9,17 @@ import XCTest
 import StevenOToole_NYCSchools
 
 final class StevenOToole_NYCSchoolsTests: XCTestCase {
+    private let decoder = JSONDecoder()
 
+    func testDecodeSchools() throws {
+       guard let data = HighSchool.mockJSON.data(using: .utf8) else {
+           XCTAssert(false)
+           return
+       }
+       let schools = try decoder.decode([HighSchool].self, from: data)
+       XCTAssert(schools.count == 3)
+       XCTAssert(schools[1].id == "21K728")
+       XCTAssert(schools[2].name == "Women's Academy of Excellence")
+  }
 
 }
