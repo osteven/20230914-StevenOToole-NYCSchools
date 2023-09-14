6c0cd9b92a00ea1ed59964842cfb0648e615731d
osteven <emadan@mac.com>
2023-09-14 12:35:07 -0700

    Initial

---
+++ StevenOToole-NYCSchools/StevenOToole-NYCSchools/StevenOToole_NYCSchoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StevenOToole_NYCSchoolsApp.swift
+//  StevenOToole-NYCSchools
+//
+//  Created by Steven O'Toole on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct StevenOToole_NYCSchoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
