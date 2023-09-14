ecc8ecc90473b1ddd68f91158c7bcc4d29e6491a
osteven <emadan@mac.com>
2023-09-14 13:26:30 -0700

    Add test target

---
+++ StevenOToole-NYCSchools/StevenOToole-NYCSchoolsTests/StevenOToole_NYCSchoolsTests.swift
@@ -0,0 +1,14 @@
+//
+//  StevenOToole_NYCSchoolsTests.swift
+//  StevenOToole-NYCSchoolsTests
+//
+//  Created by Steven O'Toole on 9/14/23.
+//
+
+import XCTest
+import StevenOToole_NYCSchools
+
+final class StevenOToole_NYCSchoolsTests: XCTestCase {
+
+
+}
